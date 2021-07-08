23905e1e2cff4051a3460ef16efd986da17448b3
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-07-08 21:56:29 +0300

    Update CategoryComponent.js

--- src/components/CategoryComponent.js
+++ src/components/CategoryComponent.js
@@ -129,7 +129,7 @@ function CategoryComponent() {
              <div className="card-body">
                <h5 className="card-title">Forest animals</h5>
                <p className="card-text">1.30 EUR</p>
-               <a href="#" className="btn btn-success" onclick="addToCart()" id="addedToCartItem">Add to cart</a>
+               <a href="#" className="btn btn-success" onclick="addToCart()" id="addedToCartItem" onClick={()=>{ alert('The item was added to cart'); }}>Add to cart</a>
              </div>
            </div>
         </div>
